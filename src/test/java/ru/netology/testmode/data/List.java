17be5167229eda7b5dcc4229ad01328334cde170
Maks-Bax <rn4aey@yandex.ru>
2022-09-15 16:50:17 +0300

    AddArtifacts

---
+++ src/test/java/ru/netology/testmode/data/List.java
@@ -0,0 +1,15 @@
+package ru.netology.testmode.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Value;
+
+@Data
+@Value
+@AllArgsConstructor
+public class RegistrationInfo {
+    private String login;
+    private String password;
+    private String status;
+
+}
